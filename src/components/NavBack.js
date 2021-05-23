2bcb6be5e7768dea89c79a9864ced4eb6706464b
Luis Valgoi <luisvalgoi@gmail.com>
2021-05-23 13:14:46 -0300

    feat: added first version of workcation second example

--- src/components/NavBack.js
+++ src/components/NavBack.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavBack() {
   return (
-    <a href="/" className="absolute top-3.5 px-6 py-3 mx-0 text-sm font-semibold text-blue-900 bg-white rounded-r-lg hover:bg-blue-800 hover:text-white focus:bg-blue-900">
+    <a href="/" className="absolute top-3.5 md:px-6 md:py-3 px-4 py-2 mx-0 text-sm font-semibold text-blue-900 bg-white rounded-r-lg hover:bg-blue-800 hover:text-white focus:bg-blue-900">
       Voltar
     </a>
   );
--- src/features/SecondExample/index.js
+++ src/features/SecondExample/index.js
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function SecondExample() {
-  return <div className="min-h-screen text-center bg-indigo-100">2nd</div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-xl px-8 py-12 mx-auto">
+        <img className="h-12 transition duration-200 ease-in-out transform hover:-translate-y-2 hover:scale-110" src="/workcation.svg" alt="logo" />
+        <img className="mt-6 rounded-md shadow-xl" src="/beach-work.jpeg" alt="beach work" />
+        <h1 className="mt-6 text-2xl font-semibold leading-tight text-gray-600 sm:text-4xl">
+          You can work from anywhere. <span className="font-semibold text-indigo-500">Take advantage of it.</span>
+        </h1>
+        <p className="mt-2 font-light text-gray-600 sm:text-md">
+          Workcation helps you find work-friendly rentals in a beautiful locations so you can enjoy some nice weathere even when you are not on vacation.
+        </p>
+        <div className="m-auto mt-4">
+          <a
+            className="inline-block px-5 py-3 text-sm font-semibold tracking-wider text-white uppercase bg-indigo-500 rounded-md shadow-lg focus:hover-bg-700 hover:bg-indigo-600"
+            href="https://www.google.com"
+          >
+            Book your Escape
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
